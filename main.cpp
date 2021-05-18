d3a86ac41ce4f69ebfd9cf4f8308e48ee94cd2b5
Walter Aoiama Nagai <walternagai@unifei.edu.br>
2021-05-17 22:31:28 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,6 @@
 using namespace std;
 
 int main() {
-  // crie duas variáveis com os nomes: peso e altura
-  
-  // crie uma variável com o nome: imc
-  
-  // leia um valor para a variável peso
-  
-  // leia um valor para a variável altura
-  
-  // calcule o valor do imc a partir da fórmula
-  
-  // mostre o valor do imc calculado
-  
+ 
   return 0;
 }
